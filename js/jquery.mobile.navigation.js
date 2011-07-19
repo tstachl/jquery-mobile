9bc449cd1e4102a36ea099e897dc9b717efb579a
Ghislain Seguin <ghislain@gmail.com>
2011-07-19 08:28:43 -0700

    Coding standards

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -884,7 +884,7 @@
 		}
 
 		// Set the location hash.
-		if( settings.changeHash !== false && url && path.stripHash(location.hash) != url ) {
+		if( settings.changeHash !== false && url && path.stripHash( location.hashi ) != url ) {
 			//disable hash listening temporarily
 			urlHistory.ignoreNextHashChange = true;
 			//update hash and history
